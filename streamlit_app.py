29118ff5fbc5db555bab4c6e0c3a55e34f4bae26
unite-deals <unitedealstest1@gmail.com>
2024-02-20 15:33:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,10 +117,10 @@ def main():
             #image_bytes = uploaded_file.read()
             #pil_image = Image.open(io.BytesIO(image_bytes))
             image = st.image(uploaded_file, caption="Graph")
-            pil_image = Image.open(io.BytesIO(image))
-            init_image = load_image(pil_image).convert("RGB")
+            pil_image = Image.open(image).convert("RGB")
+            #init_image = load_image(pil_image).convert("RGB")
             prompt = f"generate image of how this background interiors would look after {strg} also use this additional information{text_input}"
-            image = pipeline(prompt, image=init_image).images
+            image = pipeline(prompt, image=pil_image).images
             st.image(image, caption="Uploaded Image", use_column_width=True)
         else:
             time.sleep(4)
